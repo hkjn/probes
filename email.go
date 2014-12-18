1f18918cca0ca37cd6562a7132bcbb3bc8024cd8
Henrik Jonsson <me@hkjn.me>
2014-12-18 17:12:44 +0100

    adds back canonical import path

--- email.go
+++ email.go
@@ -7,7 +7,7 @@
 // required:
 //   - Config.SendGrid: sendgrid credentials
 //   - Config.Alert.Recipient: who receives the emails
-package probes
+package probes // import "hkjn.me/probes"
 
 import (
 	"bytes"
